d28e02c3f63beb16dd401753fec56bb48a2f6ca4
Mahmoud Elmasry <79700816+mhmod33@users.noreply.github.com>
2025-04-13 19:47:02 +0200

    [2629] [Time Beats: 43.17%] [Memory Beats: 30.92%] - LeetPush

---
+++ 8 Question/Function-Composition.js
@@ -0,0 +1,14 @@
+var compose = function(functions) {
+    return function(x) {
+      if(functions.length==0){
+        return x
+      }
+      else{
+        for(let i=functions.length-1; i>=0; i--){
+            x= functions[i](x)
+        }
+              return x;
+      }
+    }
+};
+
